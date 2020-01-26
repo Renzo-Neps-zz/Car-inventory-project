268ae4816a0eceb80ac953686a51ab92fcc2a5c5
Renzo Neps <39088521+Renzo-Neps@users.noreply.github.com>
2020-01-26 13:15:34 -0500

    Characteristics
    Added the characteristics for a car that we discussed

--- Project 1/Program.cs
+++ Project 1/Program.cs
@@ -4,11 +4,15 @@ namespace Project_1
 {
     class car
     {
-
-
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        public int IDNumber { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int PurchasePrice { get; set; }
+        public int SellingPrice { get; set; }
+        public int SoildPrice { get; set; }
+        public bool New { get; set; }
+        public string Status { get; set; }
+        public string Condition { get; set; }
     }
 }
