7b907e3a7d0ba49d8cf6a7b27d3b1bab889c75d6
at25892 <59653941+at25892@users.noreply.github.com>
2020-02-05 08:46:00 -0500

    Update Inventory.cs
    Display changes

--- Programs/Inventory.cs
+++ Programs/Inventory.cs
@@ -21,19 +21,42 @@ public class Inventory
 		CarInventory.Add(car);
 	}
 
-	public void EditInventory()
+	public void EditInventory(Car car)
 	{
 
 	}
 
-	public void RemoveCar()
+	public void RemoveCar(Car car)
 	{
 
 	}
 
-	public void display()
+
+	public void Display(Car car)
     {
-		Console.WriteLine("ID #: ");
-		Console.WriteLine();
+
     }
+
+	public void DisplayAll()
+	{
+		foreach (Car car in CarInventory)
+		{
+			Display(car);
+		}
+	}
 }
+
+
+/*Console.WriteLine("ID #: " + car.ID);
+
+		Console.WriteLine("{0} {1} {2} ",
+		car.Year,
+		car.Make,
+		car.Model);
+		Console.WriteLine();
+		Console.WriteLine("Status:" + car.Status);
+
+		Console.WriteLine("Condition: {0} {1}",
+			car.New, car.Condition);
+		Console.WriteLine("Miles: " + car.Miles);
+		Console.WriteLine("Color: " + car.Color);*/
--- Programs/ProgramRun.cs
+++ Programs/ProgramRun.cs
@@ -4,7 +4,22 @@ public class ProgramRun
 {
 	static void Main(string[] args)
 	{
-		Console.WriteLine("Hello world.");
-		Console.ReadLine();
+		Inventory inventory = new Inventory();
+		Car car1 = new Car();
+		Test(car1);
+		inventory.AddCar(car1);
+	}
+
+	private static void Test(Car car)
+	{
+		car.IDNumber = 369;
+		car.Make = "Ford";
+		car.Model = "F-150";
+		car.Year = 2020;
+		car.PurchasePrice = 32500;
+		car.Miles = 0;
+		car.SellingPrice = 36500;
+		car.SoldPrice = 34500;
+		car.Color = "Black";
 	}
 }
